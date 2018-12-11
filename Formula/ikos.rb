d7e0c9398ba11e3770a2fe6986e8cdb68f19545c
Maxime Arthaud <maxime.arthaud@nasa.gov>
2018-12-10 18:51:10 -0800

    ikos 2.1

--- Formula/ikos.rb
+++ Formula/ikos.rb
@@ -2,42 +2,30 @@ class Ikos < Formula
   include Language::Python::Virtualenv
   desc "Static analyzer for C/C++ based on the theory of Abstract Interpretation"
   homepage "https://github.com/nasa-sw-vnv/ikos"
-  url "https://github.com/nasa-sw-vnv/ikos/releases/download/v2.0/ikos-2.0.tar.gz"
-  sha256 "03a93794651e51e639c47b8ef821f923f7951a11669a7ad3230a68f5f27e1c5f"
+  url "https://github.com/nasa-sw-vnv/ikos/releases/download/v2.1/ikos-2.1.tar.gz"
+  sha256 "bd5e75a2a94fafc3d1cd01eb6541da458a10c7674e7ae29eb211642c526f1407"
 
   depends_on "cmake" => :build
   depends_on "apron"
   depends_on "boost"
   depends_on "gmp"
-  depends_on "llvm@4"
+  depends_on "llvm@7"
   depends_on "mpfr"
   depends_on "ppl"
   depends_on "python@2" if MacOS.version <= :snow_leopard
 
   resource "Pygments" do
-    url "https://files.pythonhosted.org/packages/71/2a/2e4e77803a8bd6408a2903340ac498cb0a2181811af7c9ec92cb70b0308a/Pygments-2.2.0.tar.gz"
-    sha256 "dbae1046def0efb574852fab9e90209b23f556367b5a320c0bcb871c77c3e8cc"
+    url "https://files.pythonhosted.org/packages/63/a2/91c31c4831853dedca2a08a0f94d788fc26a48f7281c99a303769ad2721b/Pygments-2.3.0.tar.gz"
+    sha256 "82666aac15622bd7bb685a4ee7f6625dd716da3ef7473620c192c0168aae64fc"
   end
-
-  # Fix for apron 0.9.10
-  # Fixed in upstream, remove for next version
-  patch :p0, <<~EOS
-    --- core/include/ikos/core/domain/numeric/apron.hpp
-    +++ core/include/ikos/core/domain/numeric/apron.hpp
-    @@ -87,7 +87,7 @@ inline InvPtr inv_ptr(ap_abstract0_t* inv) {
-
-     /// \\returns the size of a ap_abstract0_t
-     inline std::size_t dims(ap_abstract0_t* inv) {
-    -  return _ap_abstract0_dimension(inv).intdim;
-    +  return ap_abstract0_dimension(ap_abstract0_manager(inv), inv).intdim;
-     }
-
-     /// \\brief Add some dimensions to a ap_abstract0_t
-  EOS
 
   def install
     venv = virtualenv_create(libexec/"vendor")
     venv.pip_install resources
+
+    xy = Language::Python.major_minor_version "python"
+    pth_contents = "import site; site.addsitedir('#{lib}/python#{xy}/site-packages')\n"
+    (libexec/"vendor/lib/python#{xy}/site-packages/homebrew_deps.pth").append_lines pth_contents
 
     mkdir "build" do
       system "cmake",
@@ -50,7 +38,7 @@ class Ikos < Formula
              "-DAPRON_ROOT=#{Formula["apron"].opt_prefix}",
              "-DCUSTOM_BOOST_ROOT=#{Formula["boost"].opt_prefix}",
              "-DPYTHON_EXECUTABLE=#{libexec}/vendor/bin/python",
-             "-DLLVM_CONFIG_EXECUTABLE=#{Formula["llvm@4"].opt_prefix}/bin/llvm-config",
+             "-DLLVM_CONFIG_EXECUTABLE=#{Formula["llvm@7"].opt_prefix}/bin/llvm-config",
              ".."
       system "make", "install"
     end
