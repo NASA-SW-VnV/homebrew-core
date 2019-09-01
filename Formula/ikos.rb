20e19329ddd25d9bbc1fbc8f9c41691ec6b52863
Maxime Arthaud <maxime.arthaud@nasa.gov>
2019-08-31 20:36:53 -0700

    ikos 2.2

--- Formula/ikos.rb
+++ Formula/ikos.rb
@@ -2,21 +2,21 @@ class Ikos < Formula
   include Language::Python::Virtualenv
   desc "Static analyzer for C/C++ based on the theory of Abstract Interpretation"
   homepage "https://github.com/nasa-sw-vnv/ikos"
-  url "https://github.com/nasa-sw-vnv/ikos/releases/download/v2.1/ikos-2.1.tar.gz"
-  sha256 "bd5e75a2a94fafc3d1cd01eb6541da458a10c7674e7ae29eb211642c526f1407"
+  url "https://github.com/nasa-sw-vnv/ikos/releases/download/v2.2/ikos-2.2.tar.gz"
+  sha256 "4946d479cc8eb00b38960087c14982598b508077187162ec3b9771a82d21d4a5"
 
   depends_on "cmake" => :build
   depends_on "apron"
   depends_on "boost"
   depends_on "gmp"
-  depends_on "llvm@7"
+  depends_on "llvm@8"
   depends_on "mpfr"
   depends_on "ppl"
   depends_on "python"
 
   resource "Pygments" do
-    url "https://files.pythonhosted.org/packages/63/a2/91c31c4831853dedca2a08a0f94d788fc26a48f7281c99a303769ad2721b/Pygments-2.3.0.tar.gz"
-    sha256 "82666aac15622bd7bb685a4ee7f6625dd716da3ef7473620c192c0168aae64fc"
+    url "https://files.pythonhosted.org/packages/7e/ae/26808275fc76bf2832deb10d3a3ed3107bc4de01b85dcccbe525f2cd6d1e/Pygments-2.4.2.tar.gz"
+    sha256 "881c4c157e45f30af185c1ffe8d549d48ac9127433f2c380c24b84572ad66297"
   end
 
   def install
@@ -38,7 +38,7 @@ class Ikos < Formula
              "-DAPRON_ROOT=#{Formula["apron"].opt_prefix}",
              "-DCUSTOM_BOOST_ROOT=#{Formula["boost"].opt_prefix}",
              "-DPYTHON_EXECUTABLE=#{libexec}/vendor/bin/python",
-             "-DLLVM_CONFIG_EXECUTABLE=#{Formula["llvm@7"].opt_prefix}/bin/llvm-config",
+             "-DLLVM_CONFIG_EXECUTABLE=#{Formula["llvm@8"].opt_prefix}/bin/llvm-config",
              ".."
       system "make", "install"
     end
